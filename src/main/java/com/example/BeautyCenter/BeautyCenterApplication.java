8894425ed9ca25c22899f200fd038e4db1998c59
hungbui0905 <81497318+hungbui0905@users.noreply.github.com>
2025-05-19 20:53:07 +0700

    Initial commit

---
+++ src/main/java/com/example/BeautyCenter/BeautyCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.BeautyCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BeautyCenter/BeautyCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BeautyCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
