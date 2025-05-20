1798fe2ae67657b35a0bca6bff764ae6775d34a7
Bui Trung <trungbdhe141714@fpt.edu.vn>
2025-05-20 23:59:19 +0700

    Add home-page and front-end template

---
+++ src/main/java/com/example/BeautyCenter/controller/HomePageController.java
@@ -0,0 +1,19 @@
+package com.example.BeautyCenter.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class HomePageController {
+    @GetMapping
+    public String home() {
+        return "index-5"; // trả về file index-5.html trong templates
+    }
+}
