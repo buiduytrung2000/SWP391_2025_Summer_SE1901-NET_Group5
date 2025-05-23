ffd917ab3e08e8efc07d0f20cfdfc6a8537ca76d
hungbui0905 <81497318+hungbui0905@users.noreply.github.com>
2025-05-23 16:33:19 +0700

    Error 404

---
+++ src/main/java/vn/edu/fpt/BeautyCenter/controller/ErrorPagesController.java
@@ -0,0 +1,25 @@
+package vn.edu.fpt.BeautyCenter.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorPagesController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("jakarta.servlet.error.status_code");
+        if (statusCode != null && statusCode == 404) {
+            System.out.println("Hêllo");
+            System.out.println(statusCode);
+            return "errors/error404";
+        }
+        return "error";
+    }
+
+    public String getErrorPath() {
+        return "/error";
+    }
+
+}
