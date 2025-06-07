327e7bb55b7ca09b526eb06ff588e16d1fddd255
NguyenThuong <thuongnvhe140301@fpt.edu.vn>
2025-06-07 15:53:15 +0700

    add staff funcition and detail UI

---
+++ src/main/java/vn/edu/fpt/BeautyCenter/dto/request/StaffCreationRequest.java
@@ -0,0 +1,17 @@
+package vn.edu.fpt.BeautyCenter.dto.request;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+
+@Data
+public class StaffCreationRequest {
+    private String fullName;
+    private String email;
+    private String phone;
+    private String gender;
+    private String role;
+    private LocalDate startDate;
+    private MultipartFile avatar;
+}
