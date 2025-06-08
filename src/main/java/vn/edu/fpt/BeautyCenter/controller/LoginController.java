f361632910ca88d332826f6d07ad653dda74c04c
hungbui0905 <81497318+hungbui0905@users.noreply.github.com>
2025-06-08 12:58:35 +0700

    Edit CustomerEdit Customer

--- src/main/java/vn/edu/fpt/BeautyCenter/controller/LoginController.java
+++ src/main/java/vn/edu/fpt/BeautyCenter/controller/LoginController.java
@@ -54,6 +54,7 @@ public class LoginController {
 
         // Set session attributes
         newSession.setAttribute("user", user);
+        newSession.setAttribute("userAvatar",user.getAvatarUrl());
         newSession.setAttribute("loginTime", LocalDateTime.now());
         newSession.setAttribute("userId", user.getUserId());
         newSession.setAttribute("username", user.getUsername());
