eb315c15fd4b9a8622ef79962510d83a8c08f4cc
hungbui0905 <81497318+hungbui0905@users.noreply.github.com>
2025-06-25 23:23:10 +0700

    Product functions

--- src/main/java/vn/edu/fpt/BeautyCenter/controller/ErrorPagesController.java
+++ src/main/java/vn/edu/fpt/BeautyCenter/controller/ErrorPagesController.java
@@ -10,14 +10,16 @@ public class ErrorPagesController implements ErrorController {
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         Integer statusCode = (Integer) request.getAttribute("jakarta.servlet.error.status_code");
-        if (statusCode != null && statusCode == 404) {
-            return "errors/error404";
+        if (statusCode != null) {
+            switch (statusCode) {
+                case 404:
+                    return "errors/error404";
+                case 403:
+                    return "errors/error403";
+                case 500:
+                    return "errors/error500"; // Nếu bạn có file error500.html
+            }
         }
         return "error";
     }
-
-    public String getErrorPath() {
-        return "/error";
-    }
-
 }
